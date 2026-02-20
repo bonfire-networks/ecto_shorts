97c50562c7db9e552cb13d079658b9d015ceddf9
Mayel de Borniol <git@mayel.space>
2026-02-20 22:21:43 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/1701

--- lib/config.ex
+++ lib/config.ex
@@ -2,6 +2,6 @@ defmodule EctoShorts.Config do
   @app :ecto_shorts
 
   def repo do
-    Process.get(:ecto_repo_module) || Application.get_env(@app, :repo)
+    ProcessTree.get(:ecto_repo_module) || Application.get_env(@app, :repo)
   end
 end
