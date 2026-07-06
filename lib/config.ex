5aca05867914d44b225bc1b03c7a794a337ce210
Mayel de Borniol <git@mayel.space>
2026-07-06 01:15:15 +0100

    overload whale

--- lib/config.ex
+++ lib/config.ex
@@ -1,7 +1,15 @@
 defmodule EctoShorts.Config do
   @app :ecto_shorts
 
-  def repo do
-    ProcessTree.get(:ecto_repo_module) || Application.get_env(@app, :repo)
+  # test/dev keep the ProcessTree repo swap (multi-instance test repos); in prod a ProcessTree
+  # MISS would walk the whole process ancestry on every EctoShorts call
+  if Mix.env() in [:test, :dev] do
+    def repo do
+      ProcessTree.get(:ecto_repo_module) || Application.get_env(@app, :repo)
+    end
+  else
+    def repo do
+      Application.get_env(@app, :repo)
+    end
   end
 end
